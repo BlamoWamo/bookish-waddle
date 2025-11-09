80513b52ed7226305f0aa4dfd7cec8420680b452
Jaxson Mitchell <mitchell.jaxson@gmail.com>
2025-11-09 10:19:59 -0500

    Updated bot to match newest assignment

--- main.py
+++ main.py
@@ -2,13 +2,25 @@ from stats import get_word_count, get_char_count
 
 def get_book_text():
     with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-    return file_contents        
+        return f.read()       
 
 def main():
-    text = get_book_text()
-    count = get_word_count(text)
-    print(f"Found {count} total words")
-    get_char_count(text)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
     
+    text = get_book_text()
+
+    # word count
+    print("----------- Word Count ----------")
+    word_count = get_word_count(text)
+    print(f"Found {word_count} total words")
+
+    # character count
+    print("--------- Character Count -------")
+    char_counts = get_char_count(text)
+    for char, count in char_counts.items():
+        print(f"{char}: {count}")
+
+    print("============= END ===============")
+
 main()
--- stats.py
+++ stats.py
@@ -1,20 +1,14 @@
 def get_word_count(text):
     words = text.split()
-    word_count_int = len(words)
-    return word_count_int
+    return len(words)
 
 def get_char_count(text):
-    t_count = 0
-    p_count = 0
-    c_count = 0
+    counts = {}
 
     for char in text.lower():
-        if char == "t":
-            t_count += 1
-        elif char == "p":
-            p_count += 1
-        elif char == "c":
-            c_count += 1
+        if char.isalpha():
+            counts[char] = counts.get(char, 0) + 1
 
-    print(f"'t': {t_count}, 'p': {p_count}, 'c': {c_count}")
-    return t_count, p_count, c_count
+
+    sorted_counts = dict(sorted(counts.items(), key=lambda item: item[1], reverse=True))
+    return sorted_counts
